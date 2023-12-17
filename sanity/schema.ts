b0e1c9fbf2423edc56b91b308d04a65233cc1be4
mufaqar <mufaqar@gmail.com>
2023-12-17 18:42:07 +0500

    nav

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,7 +4,8 @@ import banner from './schema/banner'
 import whatwedo from './schema/whatwedo'
 import aboutpage from './schema/aboutpage'
 import portfolio from './schema/portfolio'
+import navmenu from './schema/navmenu'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [homepage, aboutpage, portfolio, banner, whatwedo],
+  types: [homepage, aboutpage, portfolio, banner, whatwedo, navmenu],
 }
