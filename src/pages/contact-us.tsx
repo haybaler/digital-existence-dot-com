246b7da5d493a3023b64868ee8eb10b49dc2d664
mufaqar <mufaqar@gmail.com>
2023-12-19 12:39:40 +0500

    DE

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -10,7 +10,7 @@ export default function Contact_Us() {
          <SeoMeta title="Contact Us | Digital Existence" description="Contact Us Blending digital and analog marketing activities to create holistic go-to-market activities." url="https://digital-existence.com/contact-us" />
             <main>
                 <Banner
-                    title="Contact Us | Digital Existence" />
+                    title="Contact Us" />
                 <ContactForm />
             </main>
         </>
